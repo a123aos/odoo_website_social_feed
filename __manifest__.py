ce1a5d3ede779aea867bff159c049913deb22e9e
Jeffreyytc <269692899+a123aos@users.noreply.github.com>
2026-09-25 10:38:22 +0800

    Add __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Website Social Feed",
+    "summary": "X and Threads feeds for the Odoo Website Builder",
+    "version": "19.0.1.0.0",
+    "category": "Website/Website",
+    "license": "LGPL-3",
+    "depends": ["website"],
+    "data": [
+        "views/snippets/s_x_feed.xml",
+        "views/snippets/s_threads_feed.xml",
+        "views/snippets/snippets.xml"
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "odoo_website_social_feed/static/src/snippets/s_x_feed/x_feed.js",
+            "odoo_website_social_feed/static/src/snippets/s_x_feed/000.scss",
+            "odoo_website_social_feed/static/src/snippets/s_threads_feed/threads_feed.js",
+            "odoo_website_social_feed/static/src/snippets/s_threads_feed/000.scss"
+        ],
+        "website.website_builder_assets": [
+            "odoo_website_social_feed/static/src/builder/plugins/options/x_feed_option_plugin.js",
+            "odoo_website_social_feed/static/src/builder/plugins/options/x_feed_option.xml",
+            "odoo_website_social_feed/static/src/builder/plugins/options/threads_feed_option_plugin.js",
+            "odoo_website_social_feed/static/src/builder/plugins/options/threads_feed_option.xml"
+        ]
+    },
+    "installable": true,
+    "application": false
+}
