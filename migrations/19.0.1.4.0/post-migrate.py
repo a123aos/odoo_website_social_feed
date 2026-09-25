abd243cc7f6d2f28f545a1e788957bdf331f79a1
Jeffreyytc <269692899+a123aos@users.noreply.github.com>
2026-09-25 16:25:22 +0800

    Fix Threads DOM cleanup migration

--- migrations/19.0.1.4.0/post-migrate.py
+++ migrations/19.0.1.4.0/post-migrate.py
@@ -1,3 +1,5 @@
+from lxml import etree
+
 from odoo import api, SUPERUSER_ID
 
 
@@ -6,30 +8,25 @@ def migrate(cr, version):
         return
 
     env = api.Environment(cr, SUPERUSER_ID, {})
-
     views = env["ir.ui.view"].search([
         ("arch_db", "ilike", "o_threads_feed_container"),
     ])
 
     for view in views:
         arch = view.arch_db
-        marker = '<div class="o_threads_feed_container"'
-        start = arch.find(marker)
-        if start == -1:
+        try:
+            root = etree.fromstring(arch.encode("utf-8"))
+        except (etree.XMLSyntaxError, UnicodeEncodeError):
             continue
 
-        tag_end = arch.find(">", start)
-        if tag_end == -1:
-            continue
+        changed = False
+        for node in root.xpath(
+            '//*[contains(concat(" ", normalize-space(@class), " "), '
+            '" o_threads_feed_container ")]'
+        ):
+            if len(node):
+                node[:] = []
+                changed = True
 
-        # Keep the container element and its attributes, but discard any
-        # previously rendered Threads feed that was persisted by Website Builder.
-        close = arch.find("</div>", tag_end)
-        if close == -1:
-            continue
-
-        replacement = arch[start:tag_end + 1] + "</div>"
-        new_arch = arch[:start] + replacement + arch[close + len("</div>"):]
-
-        if new_arch != arch:
-            view.write({"arch_db": new_arch})
+        if changed:
+            view.write({"arch_db": etree.tostring(root, encoding="unicode")})
