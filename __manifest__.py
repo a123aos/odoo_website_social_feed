f1494db4f94b156d557e9911d2217a74c5c5efe3
Jeffreyytc <269692899+a123aos@users.noreply.github.com>
2026-09-25 15:34:10 +0800

    Load Website Builder editor guard after Threads feed

--- __manifest__.py
+++ __manifest__.py
@@ -15,6 +15,7 @@
         "web.assets_frontend": [
             "odoo_website_social_feed/static/src/snippets/s_x_feed/000.scss",
             "odoo_website_social_feed/static/src/snippets/s_threads_feed/threads_feed.js",
+            "odoo_website_social_feed/static/src/snippets/s_threads_feed/editor_guard.js",
             "odoo_website_social_feed/static/src/snippets/s_threads_feed/000.scss"
         ],
         "website.website_builder_assets": [
